dba8f9d891a0f04f4bd7596af0e8eb34f42e2c6c
Junsuk Park <park64kr@naver.com>
2016-01-21 01:07:23 +0900

    Update minheap.py

--- minheap.py
+++ minheap.py
@@ -1,34 +1,50 @@
 class node :
-    def __init__(self, value, left=None, right=None, parent=None):
+    def __init__(self, value, position, left=None, right=None, parent=None):
         self.left = node(left)
         self.right = node(right)
         self.parent = node(parent)
         self.value = value
+        self.position = position
 
 
 class heap :
     def __init__(self,value):
         self.value = value
-        self.root = node(value)
+        self.root = node(value,1)
         self.nodeCount = 1
     def pop_node(self):
         pass
     def push_node(self,inValue):
-        currentPos = self.root
+        currentPosNode = self.root
         self.nodeCount += 1
-        while(inValue >= currentPos.value):
-            #adding dir func this here!
+        parentNodeNum = int(self.nodeCount/2)
+        #moving until currentPosNode = parentNodeNum.
+        stack = []
+        tmp = parentNodeNum
+
+        while(tmp == self.root.position):
+
+            if(tmp % 2 == 0):
+                stack.append(0)
+            else : stack.append(1)
+            tmp = tmp >> 1
+
+        while(currentPosNode.position != parentNodeNum):
+            dir = stack.pop()
             if(dir == 0):
-                currentPos = currentPos.left
-            else: currentPos = currentPos.right
-        #swap this here
-        temp = currentPos
-        
-
-    def adjust_heap(self):
-        pass
+                currentPosNode = currentPosNode.left
+            else :
+                currentPosNode = currentPosNode.right
 
 
+        newNode = node(inValue,self.nodeCount,None,None,currentPosNode)
+        currentPosNode = newNode
+        #bottom up
+        while(currentPosNode.value < currentPosNode.parent.value):
+
+            temp = currentPosNode.value
+            currentPosNode.value = currentPosNode.parent.value
+            currentPosNode.parent.value = temp
 
 def main():
     #heap array
@@ -38,5 +54,3 @@ def main():
 
     for e in range(1,arr.count()):
         root.push_node(e)
-
-
