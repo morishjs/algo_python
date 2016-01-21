63c027ff6ae5ccfbbedbb65ac64856ef2a540cc7
Junsuk Park <park64kr@naver.com>
2016-01-21 12:05:15 +0900

    simple algo

--- minheap.py
+++ minheap.py
@@ -1,8 +1,8 @@
 class node :
     def __init__(self, value, position, left=None, right=None, parent=None):
-        self.left = node(left)
-        self.right = node(right)
-        self.parent = node(parent)
+        self.left = left
+        self.right = right
+        self.parent = parent
         self.value = value
         self.position = position
 
@@ -18,39 +18,60 @@ class heap :
         currentPosNode = self.root
         self.nodeCount += 1
         parentNodeNum = int(self.nodeCount/2)
+
         #moving until currentPosNode = parentNodeNum.
         stack = []
         tmp = parentNodeNum
-
-        while(tmp == self.root.position):
-
-            if(tmp % 2 == 0):
-                stack.append(0)
-            else : stack.append(1)
-            tmp = tmp >> 1
-
-        while(currentPosNode.position != parentNodeNum):
-            dir = stack.pop()
-            if(dir == 0):
-                currentPosNode = currentPosNode.left
+        if(tmp == self.root.position):
+            if(self.nodeCount == 2):
+                newNode = node(inValue,self.nodeCount,None,None,self.root)
+                currentPosNode.left = newNode
             else :
-                currentPosNode = currentPosNode.right
+                newNode = node(inValue,self.nodeCount,None,None,self.root)
+                currentPosNode.right = newNode
+            currentPosNode = newNode
+        else:
+            while(tmp != self.root.position):
+                if(tmp % 2 == 0):
+                    stack.append(0)
+                else :
+                    stack.append(1)
+                tmp = tmp >> 1
+
+            if(len(stack) != 0):
+                lastdir = stack[0]
+
+            while(currentPosNode.position != parentNodeNum):
+                dir = stack.pop()
+                if(dir == 0):
+                    currentPosNode = currentPosNode.left
+                else :
+                    currentPosNode = currentPosNode.right
 
 
-        newNode = node(inValue,self.nodeCount,None,None,currentPosNode)
-        currentPosNode = newNode
+            newNode = node(inValue,self.nodeCount,None,None,currentPosNode)
+            if(lastdir == 0):
+                currentPosNode.left = newNode
+            else : currentPosNode.right = newNode
+            currentPosNode = newNode
+
         #bottom up
-        while(currentPosNode.value < currentPosNode.parent.value):
-
+        while(currentPosNode.value < currentPosNode.parent.value and self.root != currentPosNode):
             temp = currentPosNode.value
             currentPosNode.value = currentPosNode.parent.value
             currentPosNode.parent.value = temp
+            currentPosNode = currentPosNode.parent
 
 def main():
     #heap array
     arr = [3,2,4,5,0,3,7,1]
 
     root = heap(arr[0])
-
-    for e in range(1,arr.count()):
+    arr.pop(0)
+    for e in arr:
         root.push_node(e)
+
+
+
+if __name__ == "__main__":
+    main()
